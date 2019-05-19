b4720b73f0ad1e7898d4b04e73b854677f0be941
Michael CARBENAY <michael@commerce-hub.io>
2019-05-19 08:48:28 +0200

    Creating SDK project and its base class

---
+++ SdkCSharpSources/Altatzion.Api/ApiClientBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Altatzion.Api
+{
+    public abstract class ApiClientBase
+    {
+        protected internal ApiConnection Connection { get; private set; }
+
+        public ApiClientBase(ApiConnection apiConnection)
+        {
+            Connection = apiConnection;
+        }
+    }
+}
---
+++ SdkCSharpSources/Altatzion.Api/ApiConnection.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Altatzion.Api
+{
+    public class ApiConnection
+    {
+        public string ApiUrl { get; private set; } = "https://api.altazion.com";
+        public string Username { get; private set; }
+        public string UserSecret { get; private set; }
+
+        public ApiConnection(string userName, string secret) : this(userName, secret, null)
+        {
+
+        }
+        public ApiConnection(string userName, string secret, string url)
+        {
+            if (!string.IsNullOrEmpty(url))
+                ApiUrl = url;
+            Username = userName;
+            UserSecret = secret;
+        }
+    }
+}
